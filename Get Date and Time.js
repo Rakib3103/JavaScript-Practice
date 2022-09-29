ca9e0291552c95979bec1a62d8ee9fc5d7cfc5b6
Mazharul Islam Rakib <63310221+Rakib3103@users.noreply.github.com>
2022-09-29 18:19:34 +0600

    Add files via upload

---
+++ Get Date and Time.js
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h2>My First Javascript</h2>
+        <button type="button" onclick="document.getElementById('demo').innerHTML = Date()">Click Me</button>
+        <p id="demo"></p>
+    </body>
+</html>
